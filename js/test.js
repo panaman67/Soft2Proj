86b3e74d738dec95cee13091395994f2ccffd161
Nick Paladino <nickp@ncpholdings.org>
2019-04-04 10:12:25 -0500

    Added function to create rows in table and modified HTML to match

--- js/test.js
+++ js/test.js
@@ -1,13 +1,10 @@
-var row = document.createElement("tr");
-var data = document.createElement("td");
-var node = document.createTextNode("Test Node");
-
-row.appendChild(node);
 
 function test() {
-	var ele = document.getElementById("results").childNodes[3];
-
-	// console.log(ele);
+	var row = document.createElement("tr");
+	var data = document.createElement("td");
+	var node = document.createTextNode("Test Node");
+	row.appendChild(node);
+	var ele = document.getElementById("testing");
 
 	ele.appendChild(row);
 }
